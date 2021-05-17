0ceeccf6ddb1cbb58e905cebe0867cae775a847f
Gustavo Aldar <gustavoaldar@outlook.com>
2021-05-17 15:16:26 -0300

    update SSL

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,7 +14,7 @@ import 'dotenv/config';
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      ssl: true,
+      ssl: { rejectUnauthorized: false },
       entities: [CartItem, Cart],
       synchronize: true,
     }),
